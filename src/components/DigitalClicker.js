07624dfdd8d7fa15940b1c12271d98df1ba5173e
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2021-01-28 00:23:06 +0000

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,23 @@
-// Code DigitalClicker Component Here
+import React, {Component} from 'react'
+
+export default class DigitalClicker extends Component{
+  constructor(){
+    super()
+    this.state={timesClicked: 0}
+
+  }
+
+  handleclick=()=>{
+    this.setState(prevState => (
+      {timesClicked: prevState.timesClicked+1}
+      )
+      )
+  }
+  
+  render(){
+    console.log(this.state)
+    return(
+    <div><label>{this.state.timesClicked}</label><button onClick={this.handleclick}></button></div>
+    )
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,27 @@
-// Code YouTubeDebugger Component Here
+import React, {Component} from 'react'
+
+export default class YouTubeDebugger extends React.Component{
+    constructor(){
+      super()
+      this.state={ errors:[], user: null, settings:{bitrate:8, video:{resolution: '1080p'}}
+
+      }
+    }
+    handleClick=()=>{
+      this.setState({...this.state.settings, bitrate: 12})
+    }
+    handleClickc=() => {
+      this.setState({...this.state.settings.video, resolution:'720p'})
+    }
+    
+    render(){
+      console.log(this.state)
+      
+    return(
+    <div>
+      <button className='bitrate' onClick={this.handleClick}>bitrate</button>
+      <button className='resolution' onClick={this.handleClickc}>reso</button>
+    </div>
+    )
+  }
+}
